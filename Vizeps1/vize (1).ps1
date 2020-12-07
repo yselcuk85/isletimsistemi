f4214f9dd3ae287f763d5e7f34cce14e27791a82
yselcuk85 <yselcuk85@gmail.com>
2020-12-07 23:49:47 +0300

    İsletim sistemi
    MBLP213-Selçuk yıldız 19MY03030

---
+++ Vizeps1/vize (1).ps1
@@ -0,0 +1,31 @@
+﻿$Processes = @{}
+Get-Process -IncludeUserName | ForEach-Object {
+    $Processes[$_.Id] = $_
+}
+
+
+Get-NetTCPConnection | 
+    Where-Object { $_.LocalAddress -eq "0.0.0.0" -and $_.State -eq "Listen" } |
+    Select-Object LocalAddress,
+        LocalPort,
+        @{Name="PID";         Expression={ $_.OwningProcess }},
+        @{Name="UserName";    Expression={ $Processes[[int]$_.OwningProcess].UserName }},
+        @{Name="ProcessName"; Expression={ $Processes[[int]$_.OwningProcess].ProcessName }}, 
+        @{Name="Path"; Expression={ $Processes[[int]$_.OwningProcess].Path }} |
+    Sort-Object -Property LocalPort, UserName |
+  Format-Table -AutoSize | Out-File C:\Users\SELCUK\Desktop\Vizeps1\proces.txt -Encoding utf8
+
+
+
+
+Get-NetUDPEndpoint | 
+    Where-Object { $_.LocalAddress -eq "0.0.0.0" } |
+    Select-Object LocalAddress,
+        LocalPort,
+        @{Name="PID";         Expression={ $_.OwningProcess }},
+        @{Name="UserName";    Expression={ $Processes[[int]$_.OwningProcess].UserName }},
+        @{Name="ProcessName"; Expression={ $Processes[[int]$_.OwningProcess].ProcessName }}, 
+        @{Name="Path"; Expression={ $Processes[[int]$_.OwningProcess].Path }} |
+    Sort-Object -Property LocalPort, UserName |
+    Format-Table -AutoSize | Out-File C:\Users\SELCUK\Desktop\Vizeps1\proces.txt -Encoding utf8
+
